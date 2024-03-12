0625e0bab6c6e32d956beba4a7fe99ee4e9ac397
trobertsm <161269263+trobertsm@users.noreply.github.com>
2024-03-12 14:03:58 -0400

    Deleted excess input test items

--- GodotAquariumProject1/components/selectability.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-class_name Selectability
-
-@export var parent_object: Node2D
-@onready var selection_circle: Sprite2D = $SelectionCircle
-@onready var selection_area: Area2D = $SelectionArea
--- GodotAquariumProject1/test scenes/backgroundclickarea.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton:
-		print("Background clicked!")

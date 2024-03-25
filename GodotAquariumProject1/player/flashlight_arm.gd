057646f71517231973f0ad52fe6629f6359fe8aa
trobertsm <robertsmcmichael.thomas@gmail.com>
2024-03-25 14:22:32 -0400

    Flashlight and kelp changes
    Tried to make an "aura" around the flashlight - still looks weird. Made kelp movement into a code-based shader so its parameters can be accessed through code.

--- GodotAquariumProject1/player/flashlight_arm.gd
+++ GodotAquariumProject1/player/flashlight_arm.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
-@onready var light: PointLight2D = $PointLight2D
+@onready var beam_light: PointLight2D = $FlashlightBeamLight
+@onready var surround_light: PointLight2D = $FlashlightSurroundLight
 
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
 
 func _on_diver_flashlight_toggle() -> void:
-	light.enabled = !light.enabled
+	beam_light.enabled = !beam_light.enabled
+	surround_light.enabled = !surround_light.enabled
---
+++ GodotAquariumProject1/tank entities/kelp_2.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var kelp_sprite: Sprite2D = $KelpSprite
+
+func _ready():
+	var shader_material: ShaderMaterial = kelp_sprite.material as ShaderMaterial
+	var rand_distance: float = randf_range(1,150)
+	shader_material.set_shader_parameter("sway_distance",rand_distance)
+	var rand_scale: float = randf_range(0.4,1)
+	kelp_sprite.scale = Vector2(rand_scale,rand_scale)
